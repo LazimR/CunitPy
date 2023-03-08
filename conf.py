d4f8fa3a9d30b24b42e8129755a1da2fd7cb6d41
Kennedy de Souza <kennedo160@gmail.com>
2023-03-08 11:04:12 -0300

    Configuração do Sphinx

---
+++ conf.py
@@ -0,0 +1,30 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+html_context = {'github_url': 'https://github.com/LazimR/CunitPy'}
+
+project = 'Cunitpy'
+copyright = '2023, Lazim Claubert, Kennedy Souza'
+author = 'Lazim Claubert, Kennedy Souza'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
